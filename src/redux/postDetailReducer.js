a25904bd94cc22599168875abdd76a6abbb9b7ab
ExLuZiVe53 <imstudent27@gmail.com>
2023-10-21 17:47:32 +0200

    reloading PostDetailsPage and PostDetailsReducer

--- src/redux/postDetailReducer.js
+++ src/redux/postDetailReducer.js
@@ -1,11 +1,38 @@
 // створюємо початковий стан state
 const INITIAL_STATE = {
-  postDetails: null,
+  postDetailsData: null,
   isLoading: false,
   error: null,
 };
 
 // Ред'юсер це функція яка приймає state, action
 export const postDetailsReducer = (state = INITIAL_STATE, action) => {
-  // повертає або змінений state, або незмінений state
+  // action-> {type: 'postDetails/setIsLoading', payload: true}
+  // перевіряємо тип інструкції яка прийшла в наш ред'юсер
+  switch (action.type) {
+    case 'postDetails/setIsLoading': {
+      return {
+        ...state,
+        // єдиний спосіб коли значення прийдуть це тільки action.payload
+        isLoading: action.payload,
+      };
+    }
+
+    case 'postDetails/setPostDetails': {
+      return {
+        ...state,
+        postDetailsData: action.payload,
+      };
+    }
+
+    case 'postDetails/setError': {
+      return {
+        ...state,
+        error: action.payload,
+      };
+    }
+
+    default:
+      return state;
+  }
 };
